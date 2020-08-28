27a57f887f5d6d4e622a16cb9725e57900ca42a2
maximilianoh <maximiliano.h.belizan@gmail.com>
2020-08-28 12:53:57 -0300

    add codecov

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-    "rootDir": "./",
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "verbose": false,
-    "moduleNameMapper": {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    },
-    "transform": {
-        "^.+\\.jsx|.js?$": "babel-jest"
-    },
-    "globals": {
-        "NODE_ENV": "test"
-    },
-    "moduleFileExtensions": [
-        "js",
-        "jsx"
-    ]
-}
+  rootDir: "./",
+  roots: ["<rootDir>/src"],
+  verbose: false,
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.jsx|.js?$": "babel-jest",
+  },
+  globals: {
+    NODE_ENV: "test",
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  coverageDirectory: "./coverage/",
+  collectCoverage: true,
+};
