e6d184efcd31c4daa3a050952f9ad77a7cda937e
maximilianoh <maximiliano.h.belizan@gmail.com>
2020-08-31 17:47:07 -0300

    fix styleguidist

--- README.md
+++ README.md
@@ -1,35 +1,33 @@
 [![NPM Status](https://img.shields.io/npm/v/react-color-hook.svg?style=flat)](https://www.npmjs.com/package/react-color-hook)
 [![Build Status](https://travis-ci.com/maximilianoh/react-color-hook.svg?branch=master)](https://travis-ci.com/maximilianoh/react-color-hook)
 [![codecov](https://codecov.io/gh/maximilianoh/react-color-hook/branch/master/graph/badge.svg)](https://codecov.io/gh/babel/babel-loader)
-[![Known Vulnerabilities](https://snyk.io/test/github/maximilianoh/react-color-hook/badge.svg)](https://snyk.io/test/github/maximilianoh/react-color-hook)
+[![Vulnerabilities](https://snyk.io/test/github/maximilianoh/react-color-hook/badge.svg)](https://snyk.io/test/github/maximilianoh/react-color-hook)
 
 # react-color-hook
+
 Color picker based on https://github.com/casesandberg/react-color
 Wrote in React Hooks
 
-
-
 ## Development
-    npm start
-    
 
-## Doc project
+    npm start
+
+## Documentation and use
+
     npm run doc
 
-
-
 ## Build project
-    npm run lib
-    
-    
 
-## test
-    npm test   
-    
-    
-## examples
+    npm run lib
+
+## Test
+
+    npm test
+
+## Examples
+
     Original: http://casesandberg.github.io/react-color/#examples
-    replace 
-    import { ChromePicker } from 'react-color' 
+    replace
+    import { ChromePicker } from 'react-color'
     to
     import { ChromePicker } from "react-color-hook";
--- styleguidist.js
+++ styleguidist.js
@@ -1,48 +1,50 @@
-const styleguidist = require('react-styleguidist');
-const open = require('open');
+const styleguidist = require("react-styleguidist");
+const open = require("open");
 const styleguide = styleguidist({
-    logger: {
-        error: console.error,
-        info: console.log,
-        debug: console.log,
-        warn: console.warn,
-    },
-    title: 'React Style Guide Example',
-    components: 'src/components/**/*.jsx',
-    ignore:[
-      'src/components/chrome/ChromePointerCircle.jsx',
-      'src/components/photoshop/PhotoshopPointerCircle.jsx',
-      'src/components/photoshop/PhotoshopPointer.jsx',
-      'src/components/common/*/**'
-    ],
-    serverPort: 6065,
-    serverHost: '127.0.0.1',
-    webpackConfig: {
-        module: {
-            rules: [
-              {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: "babel-loader",
-                  options: {
-                    plugins:["@babel/plugin-syntax-dynamic-import"]
-                  }
-                }
-              }
-            ],
+  logger: {
+    error: console.error,
+    info: console.log,
+    debug: console.log,
+    warn: console.warn,
+  },
+  title: "React Style Guide Example",
+  components: "src/components/**/*.jsx",
+  ignore: [
+    "src/components/chrome/ChromePointerCircle.jsx",
+    "src/components/photoshop/PhotoshopPointerCircle.jsx",
+    "src/components/photoshop/PhotoshopPointer.jsx",
+    "src/components/common/*/**",
+  ],
+  serverPort: 6065,
+  serverHost: "127.0.0.1",
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              plugins: ["@babel/plugin-syntax-dynamic-import"],
+            },
           },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
     },
-    assetsDir: 'devWebpackServer/',
+  },
+  assetsDir: "devWebpackServer/",
 });
 
-styleguide.server(
-    (err, config) => {
-        if (err) {
-            console.log(err)
-        } else {
-            const url = `http://${config.serverHost}:${config.serverPort}`;
-            open(url);
-        }
-    }
-)
+styleguide.server((err, config) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const url = `http://${config.serverHost}:${config.serverPort}`;
+    open(url);
+  }
+});
