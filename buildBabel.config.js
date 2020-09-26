3559bedfdcdb7c0c07ed9292d72ea77599cea986
maximilianoh <maximiliano.h.belizan@gmail.com>
2020-09-26 16:50:18 -0300

    fix .css on babel build

--- buildBabel.config.js
+++ buildBabel.config.js
@@ -1,24 +1,29 @@
 module.exports = {
-	presets: [
-		"@babel/preset-react",
-		[
-			"@babel/preset-env",
-			{
-				"targets": {
-					"browsers": "last 2 versions"
-				},
-				"modules": false,
-				"loose": false
-			}
-		],
-		['minify', { builtIns: false }]
-	],
-	ignore: [
-		"./src/CallerComponents.jsx",
-		"./src/indexRender.jsx",
-		"./src/**/spec.js"
-	],
-	plugins: [
-	  "@babel/plugin-syntax-dynamic-import"
-	]
-}
+  presets: [
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          browsers: "last 2 versions",
+        },
+        modules: false,
+        loose: false,
+      },
+    ],
+    ["minify", { builtIns: false }],
+  ],
+  ignore: [
+    "./src/CallerComponents.jsx",
+    "./src/DesktopLayout.jsx",
+    "./src/example_color.js",
+    "./src/indexRender.jsx",
+    "./src/MobileLayout.jsx",
+    "./src/TabletLayout.jsx",
+    "./src/Layout.jsx",
+    "./src/**/spec.js",
+    "__snapshots__",
+    "./src/**/**.md",
+  ],
+  plugins: ["@babel/plugin-syntax-dynamic-import"],
+};
